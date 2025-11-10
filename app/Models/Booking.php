1cb27ad2e93402f466e508510f1835b767354c70
YusufCaesareaArianto <230712331@students.uajy.ac.id>
2025-11-10 22:57:42 +0700

    ada yang salah

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,24 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    use HasFactory;
-
-    protected $table = 'booking';
-    protected $primaryKey = 'id';
-    public $timestamps = false;
-
-    protected $fillable = [
-        'user_id',
-        'package_id',
-        'schedule_id',
-        'booking_date',
-        'total_person',
-        'total_price',
-        'status'
-    ];
+    //
 }
