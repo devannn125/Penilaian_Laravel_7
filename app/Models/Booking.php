4ed5c0b6e11fb0af83fbe2aa1512129f64a6a624
YusufCaesareaArianto <230712331@students.uajy.ac.id>
2025-11-11 11:45:48 +0700

    UUPDATE BOOKING
    UDAH FIX FIX

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'booking';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'package_id',
+        'schedule_id',
+        'booking_date',
+        'total_person',
+        'total_price',
+        'status'
+    ];
 }
